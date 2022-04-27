1e8b75c57151f14ff9da45c424af0943c5dc9f67
khakisage <jongjin201@gmail.com>
2022-04-27 09:36:15 +0900

    feat : 페이지 구성 및 nav 1차 작성
    없음

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav";
-import Landing from "./pages/Landing";
-import Main from "./pages/Main";
-import Board from "./pages/Board";
-import Footer from "./components/Footer";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Landing from './pages/Landing';
+import Main from './pages/Main';
+import Board from './pages/Board';
+import Footer from './components/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
       <div className="Router">
-        <Nav />
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/main" element={<Main />} />
           <Route path="/board" element={<Board />} />
         </Routes>
-        <Footer />
       </div>
     </BrowserRouter>
   );
@@ -25,7 +23,7 @@ function Router() {
 //<Routes> 내부에 유동적으로 보여줄 컴포넌트를 Route 경로와 함께 작성한다.
 
 function App() {
-  return <Router />;
+  return <Router></Router>;
 }
 
 export default App;
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,11 +1,143 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+
+// main 화면 css
+const MainContainer = styled.main`
+  width: 100%;
+  height: 100vh;
+  background: white;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainHeader = styled.header`
+  width: auto;
+  height: 5%;
+  padding: 1vh 10vw;
+  display: flex;
+  border: 10px solid pink;
+`;
+
+const MainSection = styled.section`
+  width: auto;
+  height: 100%;
+  padding: 5vh 10vw;
+  border: 10px solid yellow;
+`;
+
+const MainNav = styled.nav`
+  position: absolute;
+  top: 4vh;
+  right: 5vw;
+  border: 10px solid skyblue;
+`;
+
+const MainDiv = styled.div`
+  width: auto;
+  height: 80%;
+  padding: 5vh 10vw;
+  border: 10px solid coral;
+`;
+
+// service 소개 페이지 css
+
+const ServiceContainer = styled.main`
+  width: 100%;
+  height: 100vh;
+  background: white;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ServiceHeader = styled.header`
+  width: auto;
+  height: 5%;
+  padding: 1vh 10vw;
+  display: flex;
+  border: 10px solid pink;
+`;
+
+const ServiceSection = styled.section`
+  width: auto;
+  height: 100%;
+  padding: 5vh 10vw;
+  border: 10px solid yellow;
+`;
+
+const ServiceDiv = styled.div`
+  width: auto;
+  height: 80%;
+  padding: 5vh 10vw;
+  border: 10px solid coral;
+`;
+
+// map 페이지 css
+
+const MapContainer = styled.main`
+  width: 100%;
+  height: 100vh;
+  background: white;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MapHeader = styled.header`
+  width: auto;
+  height: 5%;
+  padding: 1vh 10vw;
+  display: flex;
+  border: 10px solid pink;
+`;
+
+const MapSection = styled.section`
+  width: auto;
+  height: 100%;
+  padding: 5vh 10vw;
+  border: 10px solid yellow;
+`;
+
+const MapDiv = styled.div`
+  width: auto;
+  height: 80%;
+  padding: 5vh 10vw;
+  border: 10px solid coral;
+`;
 
 function Main() {
   return (
-    <>
-      <div>Main</div>
-    </>
+    <div>
+      <MainContainer>
+        <MainHeader>
+          <img src="img/headerlogo.jpeg" alt="header logo"></img>
+          <MainNav></MainNav>
+        </MainHeader>
+        <MainSection>
+          <MainDiv>
+            <h1>이미지 슬라이더 들어갈 공간</h1>
+          </MainDiv>
+        </MainSection>
+      </MainContainer>
+      <ServiceContainer>
+        <ServiceHeader>
+          <img src="img/headerlogo.jpeg" alt="header logo"></img>
+        </ServiceHeader>
+        <ServiceSection>
+          <ServiceDiv>
+            <h1>서비스 소개 들어갈 공간</h1>
+          </ServiceDiv>
+        </ServiceSection>
+      </ServiceContainer>
+      <MapContainer>
+        <MapHeader>
+          <img src="img/headerlogo.jpeg" alt="header logo"></img>
+        </MapHeader>
+        <MapSection>
+          <MapDiv>
+            <h1>지도 api를 불러올 공간</h1>
+          </MapDiv>
+        </MapSection>
+      </MapContainer>
+    </div>
   );
 }
 
