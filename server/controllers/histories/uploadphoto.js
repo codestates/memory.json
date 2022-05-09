40c76ae5069824096c5f90e83c3cf9ff4d57a77e
mathisveryfun <cwyh311@naver.com>
2022-05-09 20:28:12 +0900

    feat : 단일 사진 업로드 기능 구현
    단일 사진 업로드 기능 구현하였습니다

---
+++ server/controllers/histories/uploadphoto.js
@@ -0,0 +1,24 @@
+const { photo } = require("../../models")
+
+module.exports = async (req, res) => {
+    try {
+      // // console.log("hello");
+      // // console.log(req.file);
+      // const photoInfo = await photo.create({
+      // //   // history_id: 1001,
+      //   image_name: req.file.location,
+      // });
+      // console.log(photoInfo);
+
+      console.log(req.file.location)
+      return res.status(201).json({
+        data: {
+          location: req.file.location
+        },
+        message: "ok"
+      })
+    } catch (err) {
+      console.error(err);
+      res.send("error");
+    }
+};
