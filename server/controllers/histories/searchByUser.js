863475fbd0a3f09e3875ab208ef4a904d414a000
mathisveryfun <cwyh311@naver.com>
2022-05-12 17:51:09 +0900

    feat: 내 히스토리 조회, 내가 좋아요한 페이지네이션 작업
    내 히스토리 조회, 내가 좋아요한 페이지 페이지네이션 작업 했습니다

--- server/controllers/histories/searchByUser.js
+++ server/controllers/histories/searchByUser.js
@@ -1,17 +1,32 @@
 const { history, user } = require("../../models");
+// const userAuthen = require("../authentication/userAuthen");
 
 module.exports = async (req, res) => {
   try {
     // 토큰 확인
-    /* const userInfo = await userAuthen(req, res); */
+    // const userInfo = await userAuthen(req, res);
 
     // 패스 파라미터를 조회한다.
     const user_id = req.params.userId;
-    console.log(user_id);
+    // console.log(user_id);
+
+    // 쿼리 스트링을 조회한다.
+    // console.log(req.query)
+    const pageNumber = req.query.page;
+    // console.log(pageNumber)
+    let offset = 0;
+
+    if (pageNumber > 1) {
+      offset = 2 * (pageNumber - 1); // 두개씩 잘라 보여줌.
+    }
+
 
     // 패스 파라미터와 user_id와 일치하는 유저가 쓴 히스토리들을 조회
-    const historyInfoByUser = await history.findAll({
+    const historyInfoByUser = await history.findAndCountAll({
+      raw: true,
       where: { user_id },
+      offset,
+      limit: 2
     });
     console.log(historyInfoByUser);
 
