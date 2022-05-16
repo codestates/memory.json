805c7ca89783b3198b17776a1b2a72a81596ed56
안제호 <ajh4234@gmail.com>
2022-05-16 12:08:37 +0900

    최종수정

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -251,7 +251,7 @@ function Navbar({
               onClick={modalOpener}
               style={{ color: "white", fontSize: "120%", fontWeight: "700" }}
             >
-              로그인<GoSignIn size="15" color="#fff"></GoSignIn>
+              LOGIN <GoSignIn size="15" color="#fff"></GoSignIn>
             </LoginbuttonStyle>
           ) : (
             <LoginbuttonStyle
@@ -259,7 +259,7 @@ function Navbar({
               onClick={logoutIndicator}
               style={{ color: "white", fontSize: "120%", fontWeight: "700" }}
             >
-              로그아웃<GoSignOut size="15" color="#fff"></GoSignOut>
+              LOGOUT <GoSignOut size="15" color="#fff"></GoSignOut>
             </LoginbuttonStyle>
           )}
         </FourthDiv>
